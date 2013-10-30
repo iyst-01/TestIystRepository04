5722aea5263f411a60c8131d9c6f30afc3772f0d
iyst-01 <iyst-01@yandex.ru>
2013-10-31 00:26:27 +0200

    tt2

--- TestIystRepository04/src/t1/HW3.java
+++ TestIystRepository04/src/t1/HW3.java
@@ -5,6 +5,7 @@ public class HW3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add line in hotfix");
+		System.out.println("add line in hotfix2");
 	}
 
 }
