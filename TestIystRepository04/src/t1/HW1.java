41e79324c626490e5207932b3085d98b009b56c6
iyst-01 <iyst-01@yandex.ru>
2013-10-31 00:25:08 +0200

    ttt

--- TestIystRepository04/src/t1/HW1.java
+++ TestIystRepository04/src/t1/HW1.java
@@ -5,6 +5,7 @@ public class HW1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add igromskyy");
+		System.out.println("Add igromskyy1");
 
 	}
 
