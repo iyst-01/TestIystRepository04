e43c17ab4e45aed69cf4309138795a93303704a7
iyst-01 <iyst-01@yandex.ru>
2013-10-31 20:00:47 +0200

    t1

--- TestIystRepository04/src/t1/HW1.java
+++ TestIystRepository04/src/t1/HW1.java
@@ -3,18 +3,7 @@ package t1;
 public class HW1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Add igromskyy");
-		System.out.println("Add igromskyy1");
-		System.out.println("Add igromskyy3");
-		System.out.println("Add igromskyy3");
-		System.out.println("Add igromskyy3");
-		System.out.println("Add igromskyy3");
-		System.out.println("Add igromskyy3");
-		
-		System.out.println("Add igromskyy3");
-		System.out.println("Add igromskyy3");
-
+		System.out.println("test1 - 1");
 	}
 
 }
