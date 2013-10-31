9f016674244c25f84054c4489bfb768c056e61c7
iyst-01 <iyst-01@yandex.ru>
2013-10-31 20:03:40 +0200

    t2-2

--- TestIystRepository04/src/t1/HW4.java
+++ TestIystRepository04/src/t1/HW4.java
@@ -3,10 +3,7 @@ package t1;
 public class HW4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Add line in new Branch iss53"); 
-		System.out.println("Add line 2 in new Branch iss53");
-		System.out.println("Add line 3 in new Branch iss53"); 
+		System.out.println("test2-2"); 
 	}
 
 }
