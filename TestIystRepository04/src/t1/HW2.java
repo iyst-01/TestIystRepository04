599423b89fe070acd1e2d4b7147e7ca9e88d140d
iyst-01 <iyst-01@yandex.ru>
2013-10-31 20:01:48 +0200

    t1-2

--- TestIystRepository04/src/t1/HW2.java
+++ TestIystRepository04/src/t1/HW2.java
@@ -3,17 +3,7 @@ package t1;
 public class HW2 {
 
 	public static void main(String[] args) {
-		System.out.println("To delete");
-		System.out.println("To delete");
-		
-		System.out.println("To delete");
-		System.out.println("To delete");
-		System.out.println("To delete");
-		
-		System.out.println("To delete");
-		
-		System.out.println("To delete1");
-
+		//test1-2
 	}
 
 }
